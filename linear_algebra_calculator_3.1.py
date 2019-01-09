b671d029ac36e99b028eff8f730a2e7ff8338da4
mcdowellalex <46228735+mcdowellalex@users.noreply.github.com>
2019-01-09 17:34:29 -0500

    Update linear_algebra_calculator_3.1.py

--- linear_algebra_calculator_3.1.py
+++ linear_algebra_calculator_3.1.py
@@ -23,15 +23,15 @@ class App:
         self.master.title('Linear Algebra Calculator')
         self.location = ''
         self.home_page()
-        
+
     def home_page(self):
         self.update_frame()
-        
+
         self.location = 'home page'
-            
+
         self.frame = Frame(self.master)
         self.frame.grid()
-        
+
         self.welcome = Label(self.frame, text = 'Welcome to the Linear Algebra Calculator', font = '24')
         self.welcome.grid(row = 0, columnspan = 8)
         self.choice = Label(self.frame, text = 'Please choose what you would like to calculate')
@@ -62,92 +62,92 @@ class App:
             self.det_frame.destroy()
         elif self.location == 'calculate determinant page':
             self.calc_det_frame.destroy()
-    
+
     def destroy(self):
         self.master.destroy()
-    
+
     def dot_product(self):
         self.update_frame()
-        
+
         self.location = 'dot product page'
-        
+
         self.dp_frame = Frame(self.master)
         self.dp_frame.grid()
-        
+
         self.dp_title = Label(self.dp_frame, text = 'Dot Product Calculator', font = '16')
         self.dp_title.grid(row = 0, columnspan = 2)
-        
+
         self.dp_instruction = Label(self.dp_frame, text = 'Please enter two vectors of equal length with entries separated by commas or spaces')
         self.dp_instruction.grid(row = 1, columnspan = 7)
         self.dp_instruction_2 = Label(self.dp_frame, text = 'For example: (1, 2, 3) or (1 2 3)')
         self.dp_instruction_2.grid(row = 2, columnspan = 2, sticky = W)
-        
+
         self.v1_label = Label(self.dp_frame, text = 'Vector 1:')
         self.v1_label.grid(row = 3)
         self.v1_entry = Entry(self.dp_frame, width = 20)
         self.v1_entry.grid(row = 3, column = 1)
-        
+
         self.dp_answer_label = Label(self.dp_frame, text = '', font = ('Helvetica', 24, 'bold'))
         self.dp_answer_label.grid(row = 3, column = 2, rowspan = 2)
-        
+
         self.v2_label = Label(self.dp_frame, text = 'Vector 2:')
         self.v2_label.grid(row = 4)
         self.v2_entry = Entry(self.dp_frame, width = 20)
         self.v2_entry.grid(row = 4, column  = 1)
-        
-        
+
+
         self.destroy_button = Button(self.dp_frame, text = 'Exit', command = self.destroy)
         self.destroy_button.grid(row = 7, column = 0)
-        
+
         self.calculate_dp_button = Button(self.dp_frame, text = 'Calculate', command = self.calculate_dot_product)
-        self.calculate_dp_button.grid(row = 5, column = 1)    
-        
+        self.calculate_dp_button.grid(row = 5, column = 1)
+
         self.home_button = Button(self.dp_frame, text = 'Home', command = self.home_page)
         self.home_button.grid(row = 5, column = 0)
-        
+
         self.error_label = Label(self.dp_frame, text = '')
         self.error_label.grid(row = 6, columnspan = 5)
-        
-        
+
+
     def calculate_dot_product(self):
         self.v1 = self.v1_entry.get()
         self.v2 = self.v2_entry.get()
-        
+
         self.v1 = self.v1.split(',')
         self.v1 = ' '.join(self.v1)
         self.v1 = self.v1.split(' ')
         self.v1 = [int(i) for i in self.v1]
-        
+
         self.v2 = self.v2.split(',')
         self.v2 = ' '.join(self.v2)
         self.v2 = self.v2.split(' ')
         self.v2 = [int(i) for i in self.v2]
-        
+
         if len(self.v1) == len(self.v2):
             #calculate the dot product here
             self.dp_answer = 0
             for i in range(len(self.v1)):
                 product = self.v1[i] * self.v2[i]
                 self.dp_answer = self.dp_answer + product
-                
+
             str(self.dp_answer)
-            self.dp_answer_label.config(text = self.dp_answer) 
+            self.dp_answer_label.config(text = self.dp_answer)
             self.error_label.config(text = '')
         else:
             self.error_label.config(text = 'Please enter two vectors of equal length')
-        
+
     def eigenvalues(self):
         self.update_frame()
-        
+
         self.location = 'eigenvalue page'
-        
+
         self.ev_frame = Frame(self.master)
         self.ev_frame.grid()
-        
+
         self.ev_title = Label(self.ev_frame, text = 'Eigenvalue Calculator', font = '16')
         self.ev_title.grid(row = 0, columnspan = 2)
 
-        
+
 
     def determinant(self):
         self.update_frame()
@@ -184,18 +184,18 @@ class App:
         self.error_label = Label(self.det_frame, text = '')
         self.error_label.grid(row = 4)
 
-        
+
     def calculate_determinant(self):
         self.update_frame()
 
         self.location = 'calculate determinant page'
-        
+
         self.calc_det_frame = Frame(self.master)
         self.calc_det_frame.grid()
 
         #logic for whether there was something entered for matrix size
         self.matrix_size = int(self.matrix_size_entry.get())
-        
+
         if isinstance(self.matrix_size, int):
             #row 0
             self.calc_det_title = Label(self.calc_det_frame, text = 'Calculate the Determinant', font = '16')
@@ -207,7 +207,7 @@ class App:
 
             self.back_button = Button(self.calc_det_frame, text = 'Back', command = self.determinant)
             self.back_button.grid(row = 1, column = 1)
-            
+
             self.destroy_button = Button(self.calc_det_frame, text = 'Exit', command = self.destroy)
             self.destroy_button.grid(row = 1, column = 2)
 
@@ -217,9 +217,9 @@ class App:
 
             self.back_button = Button(self.calc_det_frame, text = 'Back', command = self.determinant)
             self.back_button.grid(row = 1, column = 0)
-            
 
-        
+
+
 
 
 '''
