4618b0f4e399a4f0334ad3e8973f8af3cce37b32
mcdowellalex <46228735+mcdowellalex@users.noreply.github.com>
2019-01-14 13:33:44 -0500

    Update calculate determinant testing.py
    trying to make a copy of the list
    rows without having it point to the
    same memory location
    so I can make alterations to the
    copy without changing the original

--- calculate determinant testing.py
+++ calculate determinant testing.py
@@ -1,30 +1,53 @@
-
+'''
+###################################
+trying to make a copy of the list
+rows without having it point to the
+same memory location
+so I can make alterations to the
+copy without changing the original
+###################################
+'''
 
 def calculate_determinant(rows):
+    ans = 0
     if len(rows[0]) == 2:
-        print(rows)
+        print('p')
         ans = (rows[0][0] * rows[1][1]) - (rows[0][1] * rows[1][0]) #basic determinant calculation = a*d - b*c
-        return ans
+        
     else:
-        print(rows)
         coefficients = rows[0] #first row is all the coefficients for the determinant
         rows.pop(0) #remove the row of coefficients
-
+        
+        
         for j in range(len(rows[0])):
-            if j % 2 == 0: #the number is even so the coefficient is even
-                new_rows = rows
-                for i in new_rows:
-                    i.pop(j)
-                             
-                coefficients[j] * calculate_determinant(new_rows)
-                print(rows)
+            if j % 2 == 0: #the number is even so the coefficient is even      
+                new_rows = rows[:]
+                
+                print('new',new_rows)
+                print('rows',rows)
+                
+                for i in range(len(rows)):
+                    new_rows[i].pop(j)
+                    print('new',new_rows)
+                    print('rows',rows)
+                    
+                ans = ans + (coefficients[j] * calculate_determinant(new_rows))
+                print('even',ans)
+                print('new',new_rows)
+                print('rows',rows)
             else: #the number is odd so the coefficient is odd
-                new_rows = rows
-                for i in new_rows:
-                    i.pop(j)
+                new_rows = rows.copy()
 
-                -(coefficients[j]) * calculate_determinant(new_rows)
+                for i in range(len(rows)):
+                    new_rows[i].pop(j)
+                                            
+                ans = ans + (-(coefficients[j]) * calculate_determinant(new_rows))
+                
+                      
+    return ans                
+        
                
 
-rows = [[1,2,3],[1,2,3],[1,2,3]]
-calculate_determinant(rows)
+rows = [[1,2,4],[2,1,3], [4,4,5]]
+answer = calculate_determinant(rows)
+print('answer is:' , answer)
